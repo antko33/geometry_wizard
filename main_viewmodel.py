917b526ac9b0ee8a60ffd285baee24e944490f35
Anton K <antko33@gmail.com>
2025-03-04 17:30:39 +0300

    Added calculations

--- main_viewmodel.py
+++ main_viewmodel.py
@@ -4,26 +4,30 @@ import numpy as np
 
 
 class MainViewModel:
-    t = 0
-    l_st = 0
-    l_ot = 0
-    r_vn = 0
+    def __init__(self):
+        self.t = 0
+        self.l_st = 0
+        self.l_ot = 0
+        self.r_vn = 0
 
-    r_os = 0
-    a = 0
-    s_x = 0
-    s_y = 0
-    x_c = 0
-    y_c = 0
-    j_x = 0
-    j_y = 0
-    j_p = 0
-    w_x = 0
-    w_y = 0
-    i_x = 0
-    i_y = 0
-    h = 0
-    b = 0
+        self.r_os = 0
+        self.a = 0
+        self.s_x = 0
+        self.s_y = 0
+        self.x_c = 0
+        self.y_c = 0
+        self.j_x = 0
+        self.j_y = 0
+        self.j_p = 0
+        self.w_x = 0
+        self.w_y = 0
+        self.i_x = 0
+        self.i_y = 0
+        self.h = 0
+        self.b = 0
+
+        self.__alpha1 = math.radians(60)  # Центральный угол гиба (60°)
+        self.__alpha2 = math.radians(90)  # Угол гиба отвеса (90°)
 
     __alpha1 = math.radians(60)  # Центральный угол гиба (60°)
     __alpha2 = math.radians(90)  # Угол гиба отвеса (90°)
@@ -33,12 +37,12 @@ class MainViewModel:
         self.r_os = self.r_vn + self.t / 2
 
         self.__calculate_profile_points()
-
         self.__calculate_area()
-
         self.__calculate_static_moments()
-
         self.__calculate_center_of_gravity()
+        self.__calculate_moments_of_inertia()
+        self.__calculate_section_modulus()
+        self.__calculate_dimensions()
 
     # Определение координат точек профиля для дальнейших расчетов
     def __calculate_profile_points(self):
@@ -129,7 +133,7 @@ class MainViewModel:
         # 5. Прямоугольник отгиба
         a5 = self.l_ot * self.t
 
-        self.__a = (a1 + a2 + a3 + a4 + a5) / 100  # переводим в см²
+        self.a = (a1 + a2 + a3 + a4 + a5) / 100  # переводим в см²
 
     def __calculate_static_moments(self):
         """Расчет статических моментов площади поперечного сечения"""
@@ -168,3 +172,61 @@ class MainViewModel:
         # Формулы: xc = Sy/A, yc = Sx/A
         self.x_c = self.s_y / self.a  # см
         self.y_c = self.s_x / self.a  # см
+
+    def __calculate_moments_of_inertia(self):
+        """Расчет осевых моментов инерции"""
+        # Метод: разбиваем на простые фигуры, находим моменты инерции каждой,
+        # и суммируем с учетом положения каждой фигуры относительно центра тяжести
+
+        # Для осевых моментов инерции используем:
+        # Jx = 1/12 * ∑[(y_i² + y_i*y_{i+1} + y_{i+1}²) * (x_i*y_{i+1} - x_{i+1}*y_i)]
+        # Jy = 1/12 * ∑[(x_i² + x_i*x_{i+1} + x_{i+1}²) * (x_i*y_{i+1} - x_{i+1}*y_i)]
+
+        points = np.array(self.__points)
+        x = points[:, 0]
+        y = points[:, 1]
+
+        j_x = 0
+        j_y = 0
+
+        n = len(points)
+        for i in range(n):
+            j = (i + 1) % n
+            j_x += (y[i] ** 2 + y[i] * y[j] + y[j] ** 2) * (x[i] * y[j] - x[j] * y[i])
+            j_y += (x[i] ** 2 + x[i] * x[j] + x[j] ** 2) * (x[i] * y[j] - x[j] * y[i])
+
+        # Делим на 12 и берем абсолютное значение
+        self.j_x = abs(j_x) / 12 / 10000  # переводим в см⁴
+        self.j_y = abs(j_y) / 12 / 10000  # переводим в см⁴
+
+        # Перенос моментов инерции к центральным осям (если нужно)
+        # Jx_c = Jx - A * yc²
+        # Jy_c = Jy - A * xc²
+        self.j_x = self.j_x - self.a * (self.y_c ** 2)
+        self.j_y = self.j_y - self.a * (self.x_c ** 2)
+
+    def __calculate_section_modulus(self):
+        """Расчет моментов сопротивления"""
+        # Находим наиболее удаленные точки от центра тяжести
+        points = np.array(self.__points)
+        x = points[:, 0] / 10  # переводим в см
+        y = points[:, 1] / 10  # переводим в см
+
+        x_dists = abs(x - self.x_c)
+        y_dists = abs(y - self.y_c)
+
+        x_max = max(x_dists)
+        y_max = max(y_dists)
+
+        # Расчет моментов сопротивления
+        self.w_x = self.j_x / y_max  # см³
+        self.w_y = self.j_y / x_max  # см³
+
+    def __calculate_dimensions(self):
+        """Расчет высоты и ширины профиля"""
+        points = np.array(self.__points)
+        x = points[:, 0]
+        y = points[:, 1]
+
+        self.h = max(y) - min(y)  # мм
+        self.b = max(x) - min(x)  # мм
